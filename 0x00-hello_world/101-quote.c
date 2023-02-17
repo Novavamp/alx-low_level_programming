6f40469e79c5d89813552debab15a7d485d6e80e
Novavamp <preciousgabraels2@gmail.com>
2023-02-17 01:47:20 -0800

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * A C program that prints exactly and that piece of
+ * art is useful - Dora Korper, 2015-10-19
+ * Return = 1 (Failure)
+ */
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	return (1);
+}
