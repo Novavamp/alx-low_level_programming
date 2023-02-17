0ed50abf33372dfa1a7f14bfe46fad79b9f11010
Novavamp <preciousgabraels2@gmail.com>
2023-02-16 17:32:38 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: ALways 0 (Success)
+ */
+int main(void)
 {
-        char Mystr[] = "Programming is like building a multilingual puzzle";
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
-        puts(Mystr);
-
-        return (0);
+	puts(Mystr);
+	return (0);
 }
