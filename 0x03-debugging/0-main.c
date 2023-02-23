0c91a4e3d1d1cb2340834cb11380682c227dbc33
Novavamp <preciousgabraels2@gmail.com>
2023-02-23 12:43:54 -0800

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,17 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 98;
-        positive_or_negative(i);
+	i = 98;
 
-        return (0);
+	positive_or_negative(i);
+	printf("\n");
+	return (0);
 }
