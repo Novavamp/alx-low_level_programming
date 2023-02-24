f2771bb1fd59b0a136cd4c0624867973a595a303
Novavamp <preciousgabraels2@gmail.com>
2023-02-23 17:34:32 -0800

    Print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - A function that prints numbers with a new line
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 && num != 4)
+		_putchar((num % 10) + '0');
+	}
+
+	_putchar('\n');
+}
