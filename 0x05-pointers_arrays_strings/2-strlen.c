8845ca46d3a20ca95cf3237fd07dad16d68edb5f
Novavamp <preciousgabraels2@gmail.com>
2023-02-27 10:30:36 -0800

    A function that returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - a func that returns the length of a string
+ * @s: the string in type char
+ * Return: 0.
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for (; *s++;)
+		i++;
+	return (a);
+}
