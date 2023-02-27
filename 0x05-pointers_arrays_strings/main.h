37cd457450eacc5ffdc0d28a917d32cf6d0a89ef
Novavamp <preciousgabraels2@gmail.com>
2023-02-27 14:44:16 -0800

    done

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@
  */
 
 #include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -20,7 +21,7 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *AOA_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
 #endif
