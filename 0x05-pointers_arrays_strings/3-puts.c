64706edb2ebe2740f39ee67f68bad0776e263704
Novavamp <preciousgabraels2@gmail.com>
2023-02-27 10:50:23 -0800

    printing a string with a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - A function that prints a string wiht a new line
+ * @str: the string
+ * Return: 0.
+ */
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
+
