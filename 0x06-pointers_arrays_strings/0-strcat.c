02392e22b62e779ad4b9a2a1618379ec059bb0ce
Novavamp <preciousgabraels2@gmail.com>
2023-03-04 01:54:17 -0800

    A function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strcat - A function that concatenates two strings
+ * @src: String to be appended
+ * @dest: String to be added to
+ * Return: A pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	_strcat(dest, scr);
+}
