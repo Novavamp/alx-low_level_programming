af63d77575358ced4a0001c130fdf76aacb9ec9d
Novavamp <preciousgabraels2@gmail.com>
2023-03-06 14:10:12 -0800

    A function that files memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,16 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = 'b';
+	unsigned int i;
+	char fill = b;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
-
-	return (memory);
+	while (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			s[i] = bytes;
+		}
+		n--;
+	}
+	return (s);
 }
