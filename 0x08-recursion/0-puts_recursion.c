75a94209745fba1f44ded81fccf06ae97bb544d6
Novavamp <preciousgabraels2@gmail.com>
2023-03-10 07:44:03 -0800

    A function that prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - The function prints a string
+ * @s: The string to be printed
+ *
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s);
+{
+	char s;
+
+	_puts_recursion("%s\n", s);
+	return (0);
+}
