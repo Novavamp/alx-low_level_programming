91802ad94e5f3e7c76f4e439689fb484870e220a
Novavamp <preciousgabraels2@gmail.com>
2023-03-10 09:59:03 -0800

    A function that prints the factorial of numbers

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,17 +8,17 @@
 
 int factorial(int n)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-	else if (n == 0)
-	{
-		return (1);
-	}
-	else if (n > 0)
+
+	if (n > 0)
 	{
 		return (n * factorial(n - 1));
 	}
-	return;
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
 }
