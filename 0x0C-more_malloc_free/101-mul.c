44bc52ad088128ba12a9366cfc06e07eb358dd77
Novavamp <preciousgabraels2@gmail.com>
2023-03-16 17:55:45 -0700

    A program that multiplies 2 numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,43 +2,42 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 98
- */
 int main(int argc, char *argv[]) {
+    char *num1_str, *num2_str;
+    int num1, num2, result;
+    int i;
+
     if (argc != 3) {
         printf("Error\n");
         return 98;
     }
 
-    char *num1_str = argv[1];
-    char *num2_str = argv[2];
+    num1_str = argv[1];
+    num2_str = argv[2];
 
-    for (int i = 0; num1_str[i] != '\0'; i++) {
+    for (i = 0; num1_str[i] != '\0'; i++) {
         if (!isdigit(num1_str[i])) {
             printf("Error\n");
             return 98;
         }
     }
 
-    for (int i = 0; num2_str[i] != '\0'; i++) {
+    for (i = 0; num2_str[i] != '\0'; i++) {
         if (!isdigit(num2_str[i])) {
             printf("Error\n");
             return 98;
         }
     }
 
-    int num1 = atoi(num1_str);
-    int num2 = atoi(num2_str);
+    num1 = atoi(num1_str);
+    num2 = atoi(num2_str);
 
     if (num1 <= 0 || num2 <= 0) {
         printf("Error\n");
         return 98;
     }
 
-    int result = num1 * num2;
+    result = num1 * num2;
 
     printf("%d\n", result);
 
