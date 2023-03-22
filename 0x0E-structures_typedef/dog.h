fe8644bd5c5e4f9afd70a93c7a8766282cee3158
Novavamp <preciousgabraels2@gmail.com>
2023-03-21 19:29:25 -0700

    Defining a new struct named dog.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - defines a new type of struct
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ *
+ * Description: The `struct dog` is a C structure that represents a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
