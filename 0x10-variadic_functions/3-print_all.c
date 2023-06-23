ec36f47d1e5bfc3862c80a5a71d14172413a9644
Novavamp <preciousgabraels2@gmail.com>
2023-06-23 07:59:26 -0700

    Variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,47 +1,45 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
  * print_all - function that prints anything
- * @format: list of arguments passed to the function
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
-	va_list lists;
-	char *s;
-	char ch;
-	int i = 0;
+	va_list args;
+	unsigned int i = 0;
+	char *s, *separator = "";
 
-	va_start(lists, format);
+	va_start(args, format);
 
 	while (format && format[i])
 	{
 		switch (format[i])
 		{
 			case 'c':
-				ch = va_arg(lists, int);
-				printf("%c", ch);
+				printf("%s%c", separator, va_arg(args, int));
 				break;
 			case 'i':
-				printf("%d", va_arg(lists, int));
+				printf("%s%d", separator, va_arg(args, int));
 				break;
 			case 'f':
-				printf("%f", va_arg(lists, double));
+				printf("%s%f", separator, va_arg(args, double));
 				break;
 			case 's':
-				s = va_arg(lists, char *);
+				s = va_arg(args, char *);
 				if (s == NULL)
-					printf("(nil)");
-				else
-					printf("%s", s);
+					s = "(nil)";
+				printf("%s%s", separator, s);
 				break;
 			default:
 				i++;
 				continue;
 		}
-		if (format[i + 1] != '\0')
-			printf(", ");
+		separator = ", ";
 		i++;
 	}
 	printf("\n");
-	va_end(lists);
+	va_end(args);
 }
